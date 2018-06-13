8d8c47c6ca839ccf4f8efbd54c08864fbee77dd1
Marcel Ramos <LiNk-NY@users.noreply.github.com>
2018-06-13 16:43:08 -0400

    add formats to _build.sh, closes #30 (#32)
    * pdf
    * EPUB

--- _build.sh
+++ _build.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
+
